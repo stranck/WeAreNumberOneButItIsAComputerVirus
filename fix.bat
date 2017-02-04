1318cba950664bc70c81267bb370682b6a94a357
Stranck <strdjn@gmail.com>
2017-02-04 19:39:22 +0100

    Added this file for fix the virus

---
+++ fix.bat
@@ -0,0 +1,30 @@
+@echo off
+title=Fix
+echo [%time%] Fixing WeAreNumberOneButItIsAComputerVirus...
+
+::kill the process in the right order
+taskkill /IM wscript.exe /F
+taskkill /IM cmd.exe /FI "WINDOWTITLE ne Fix"
+taskkill /IM java.exe /F
+taskkill /IM javaw.exe /F
+::delate the startup file
+
+@del "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\windowsupdate.vbs"
+
+::remove .robbierotten to your file name
+timeout 3 > null
+java -jar "C:\Users\%username%\Documents\party\wano.jar" a
+
+::delete the virus data
+rmdir "C:\Users\%username%\Documents\party" /S /Q
+
+::more stuff
+@del command
+@del other.bat
+echo [%time%] Done!
+echo.
+echo Remember, go support our Robbie Rotten number one! ;) https://www.gofundme.com/2tm9tqk
+start https://www.gofundme.com/2tm9tqk
+echo.
+echo.
+pause
